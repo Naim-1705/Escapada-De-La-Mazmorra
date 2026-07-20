7195e80ab150b9534a38d88faccaac6c4ef46ad1
Naim <rolonnaim32@gmail.com>
2026-07-20 18:07:53 -0300

    Add files via upload

--- Monstruo.h
+++ Monstruo.h
@@ -2,21 +2,30 @@
 #define MONSTRUO_H_INCLUDED
 #include <cstring>
 
+#include "archivoMonstruo.h"
+
 class Monstruo{
 private:
+    int id;
     char nombre[20];
     int vida;
     int ataque;
     bool estado;
 public:
     ///CONSTRUCTOR
-    Monstruo(int v = 100, int a = 25, bool e = true){
+    Monstruo(int v = 100, int a = 25, bool e = true, int i = -1){
         vida = v;
         ataque = a;
         estado = e;
+        id = i;
     }
 
     ///SETTERS
+    void set_id(int _id){
+        if(_id >= 0){
+            id = _id;
+        }
+    }
     void set_vida(int _vida){
         if(_vida >= 0){
             vida = _vida;
@@ -34,10 +43,52 @@ public:
     }
 
     ///GETTERS
+    int getId(){return id;}
     int get_vida(){return vida;}
     int get_ataque(){return ataque;}
     char* get_nombre(){return nombre;}
     bool get_estado(){return estado;}
+
+    bool cargar(){
+        Monstruo mon;
+        ArchivoMonstruo archi;
+
+        int contador = archi.cantidadMonstruo();
+
+        cout << "Ingrese el nombre del Monstruo: ";
+        cin >> nombre;
+        cout << endl;
+
+        for(int i = 0; i < contador; i++){
+            if(strcmp(mon.get_nombre(),nombre)==0 && mon.get_estado()){
+                cout << "Este Monstruo ya existe" << endl;
+                return false;
+            }
+        }
+
+        id = contador + 1;
+
+        cout << "Ingrese la vida del Monstruo: ";
+        cin >> vida;
+        cout << endl;
+        while(vida < 0){
+            cout << "Esta vida no es valido! Ingrese otro valor valido: ";
+            cin  >> vida;
+            cout << endl;
+        }
+
+        cout << "Ingrese el Damage del Monstruo: ";
+        cin >> ataque;
+        cout << endl;
+        while(ataque < 0){
+            cout << "Este Damage no es valido! Ingrese otro valor valido: ";
+            cin  >> ataque;
+            cout << endl;
+        }
+
+        estado = true;
+        return true;
+    }
 };
 
 #endif // MONSTRUO_H_INCLUDED
--- archivoMonstruo.cpp
+++ archivoMonstruo.cpp
@@ -4,32 +4,32 @@
 
 using namespace std;
 
-#include "archivoConsulta.h"
+#include "archivoMonstruo.h"
 
-int ArchivoConsulta::agregarRegistro(){
-    Consulta reg;
+int ArchivoMonstruo::agregarMonstruo(){
+    Monstruo reg;
     int registro;
-    FILE *pCon;
+    FILE *pMon;
 
-    pCon = fopen(nombre,"ab");
+    pMon = fopen(nombre,"ab");
 
-    if(pCon == nullptr){
+    if(pMon == nullptr){
         return -1;
     }
 
     if(!reg.cargar()){
         cout << "No se cargo correctamente el registro" << endl;
-        return -1;
+        return -2;
     }
 
-    registro = fwrite(&reg,tamanioRegistro,1,pCon);
+    registro = fwrite(&reg,tamanioRegistro,1,pMon);
 
-    fclose(pCon);
+    fclose(pMon);
     return registro;
 }
 
-bool ArchivoConsulta::bajaLogica(int id){
-    Consulta reg;
+bool ArchivoMonstruo::bajaLogica(int id){
+    Monstruo reg;
     int pos = buscarPosicion(id);
     if(pos == -1)return false;
 
@@ -40,460 +40,132 @@ bool ArchivoConsulta::bajaLogica(int id){
     }
     else{
         reg.setEstado(false);
-        return modificarRegistro(reg,pos);
+        return modificarMonstruo(reg,pos);
     }
 }
 
 
-int ArchivoConsulta::modificarRegistro(Consulta reg,int pos){
-    FILE *pCon;
-    pCon= fopen(nombre,"rb+");
-    if(pCon == nullptr){
+int ArchivoMonstruo::modificarMonstruo(Monstruo reg,int pos){
+    FILE *pMon;
+    pMon= fopen(nombre,"rb+");
+    if(pMon == nullptr){
         return -1;
     }
 
-    fseek(pCon,pos * tamanioRegistro,0);
-    int escribio = fwrite(&reg,tamanioRegistro,1,pCon);
+    fseek(pMon,pos * tamanioRegistro,0);
+    int escribio = fwrite(&reg,tamanioRegistro,1,pMon);
 
-    fclose(pCon);
+    fclose(pMon);
     return escribio;
 }
 
-int ArchivoConsulta::buscarPosicion(int id){
-    Consulta reg;
+int ArchivoMonstruo::buscarPosicion(int id){
+    Monstruo reg;
     int pos=0;
-    FILE *pCon;
+    FILE *pMon;
 
-    pCon = fopen(nombre,"rb");
+    pMon = fopen(nombre,"rb");
 
-    if(pCon == nullptr){
+    if(pMon == nullptr){
         return -1;
     }
 
-    while(fread(&reg,tamanioRegistro,1,pCon)==1){
-        if(reg.getNumeroConsulta() == id){
-            fclose(pCon);
+    while(fread(&reg,tamanioRegistro,1,pMon)==1){
+        if(reg.getId()==id){
+            fclose(pMon);
             return pos;
         }
         pos++;
     }
-    fclose(pCon);
+    fclose(pMon);
     return -2;
 }
 
-Consulta ArchivoConsulta::leerRegistro(int pos){
-    Consulta reg;
-    FILE *pCon;
-    reg.setNumeroConsulta(-1);
+Monstruo ArchivoMonstruo::leerMonstruo(int pos){
+    Monstruo reg;
+    FILE *pMon;
+    reg.set_id(-1);
 
-    pCon = fopen(nombre,"rb");
+    pMon = fopen(nombre,"rb");
 
-    if(pCon == nullptr){
+    if(pMon == nullptr){
         return reg;
     }
 
-    fseek(pCon,pos * tamanioRegistro,0);
-    fread(&reg,tamanioRegistro,1,pCon);
+    fseek(pMon,pos * tamanioRegistro,0);
+    fread(&reg,tamanioRegistro,1,pMon);
 
-    fclose(pCon);
+    fclose(pMon);
     return reg;
 }
 
-int ArchivoConsulta::cantidadRegistro(){
-    Consulta reg;
-    FILE *pCon;
+int ArchivoMonstruo::cantidadMonstruo(){
+    Monstruo reg;
+    FILE *pMon;
 
-    pCon = fopen(nombre,"rb");
+    pMon = fopen(nombre,"rb");
 
-    if(pCon == nullptr){
+    if(pMon == nullptr){
         return -1;
     }
 
-    fseek(pCon,0,2);
-    int tam = ftell(pCon);
-    fclose(pCon);
+    fseek(pMon,0,2);
+    int tam = ftell(pMon);
+    fclose(pMon);
 
     int cantReg;
     cantReg = tam / tamanioRegistro;
     return cantReg;
 }
 
-bool ArchivoConsulta::listarRegistros(){
-    Consulta reg;
-    FILE *pCon;
+bool ArchivoMonstruo::copiaSeguridad(){
+    Monstruo reg;
 
-    pCon = fopen(nombre,"rb");
-    if(pCon == nullptr){
-        return false;
-    }
+     FILE *pMo;
+     FILE *pMon;
 
-    int cant = 1;
+     pMo = fopen("copiaMonstruos.dat","wb");
+     pMon = fopen(nombre,"rb");
 
-    cout << "== LISTADO DE REGISTROS ==" << endl;
-    while(fread(&reg,tamanioRegistro,1,pCon)==1){
-        if(reg.getEstado()){
-            cout << "REGISTRO N"<<cant<< ":"<<endl;
-            reg.mostrar();
-            cout << endl;
-            cant++;
-        }
-    }
-    fclose(pCon);
-    return true;
-}
-
-bool ArchivoConsulta::listarRegistrosInactivos(){
-    Consulta reg;
-    FILE *pCon;
-
-    pCon = fopen(nombre,"rb");
-    if(pCon == nullptr){
-        return false;
-    }
-
-    int cant = 1;
-
-    cout << "== LISTADO REGISTROS INACTIVOS ==" << endl;
-    while(fread(&reg,tamanioRegistro,1,pCon)==1){
-        if(reg.getEstado()==false){
-            cout << "REGISTRO N"<<cant<<":"<<endl;
-            reg.mostrar();
-            cout << endl;
-            cant++;
-        }
-    }
-    fclose(pCon);
-    return true;
-}
-
-bool ArchivoConsulta::listarFecha(){
-    Consulta aux;
-    int cant = cantidadRegistro();
-    if(cant <= 0){
-        cout << "no hay registros" << endl;
-        return false;
-    }
-
-    Consulta *registro;
-    registro = new Consulta[cant];
-
-    if(registro == nullptr){
-        cout << "Error de memoria" << endl;
-        return false;
-    }
-
-    ///Cargar vector
-    for(int y = 0; y < cant ; y++){
-        registro[y] = leerRegistro(y);
-    }
-
-    ///Ordenar vector
-    for(int i = 0; i < cant; i++){
-        for(int j = 0; j < cant; j++){
-            if(registro[i].getFechaConsulta().getAnio() > registro[j].getFechaConsulta().getAnio() && j > i){
-                aux = registro[i];
-                registro[i] = registro[j];
-                registro[j] = aux;
-            }
-            if(registro[i].getFechaConsulta().getAnio() == registro[j].getFechaConsulta().getAnio() && registro[i].getFechaConsulta().getMes() > registro[j].getFechaConsulta().getMes() && j > i){
-                aux = registro[i];
-                registro[i] = registro[j];
-                registro[j] = aux;
-            }
-            if(registro[i].getFechaConsulta().getAnio() == registro[j].getFechaConsulta().getAnio() && registro[i].getFechaConsulta().getMes() == registro[j].getFechaConsulta().getMes() && registro[i].getFechaConsulta().getDia() > registro[j].getFechaConsulta().getDia() && j > i){
-                aux = registro[i];
-                registro[i] = registro[j];
-                registro[j] = aux;
-            }
-        }
-    }
-
-    int cont = 1;
-
-    cout<<"== LISTADO POR ORDEN DE FECHA REGISTRO =="<<endl;
-    for(int x = 0; x < cant; x++){
-        if(registro[x].getEstado()==true){
-            cout << "REGISTRO N" <<cont<< ":" << endl;
-            registro[x].mostrar();
-            cout << endl;
-            cont++;
-        }
-    }
-
-    delete[] registro;
-    return true;
-}
-
-bool ArchivoConsulta::listarPrecio(){
-    Consulta aux;
-    int cant = cantidadRegistro();
-    if(cant <= 0){
-        cout << "no hay registros" << endl;
-        return false;
-    }
-
-    Consulta *precio;
-    precio = new Consulta[cant];
-
-    if(precio == nullptr){
-        cout << "Error de memoria" << endl;
-        return false;
-    }
-
-    ///Cargar vector
-    for(int y = 0; y < cant ; y++){
-        precio[y] = leerRegistro(y);
-    }
-
-    ///Ordenar vector
-    for(int i = 0; i < cant; i++){
-        for(int j = 0; j < cant; j++){
-            if(precio[i].getPrecioConsulta() > precio[j].getPrecioConsulta() && j > i){
-                aux = precio[i];
-                precio[i] = precio[j];
-                precio[j] = aux;
-            }
-        }
-    }
-
-    int cont = 1;
-
-    cout<<"== LISTADO POR ORDEN DE FECHA REGISTRO =="<<endl;
-    for(int x = 0; x < cant; x++){
-        if(precio[x].getEstado()==true){
-            cout << "REGISTRO N" <<cont<< ":" << endl;
-            precio[x].mostrar();
-            cout << endl;
-            cont++;
-        }
-    }
-
-    delete[] precio;
-    return true;
-}
-
-bool ArchivoConsulta::buscarConsultaPorID(){
-    Consulta reg;
-    int id;
-    cout << "Ingreses el ID de la consulta: ";
-    cin >> id;
-
-    int pos = buscarPosicion(id);
-    if(pos == -1){
-        cout << "Error en memoria" << endl;
-        return false;
-    }
-    if(pos == -2){
-        cout << "El ID ingresado no existe dentro del sistema" << endl;
-        return false;
-    }
-
-    reg = leerRegistro(pos);
-
-    cout << "== CONSULTA BUSCADA POR ID ==" << endl;
-
-    reg.mostrar();
-    return true;
-}
-
-bool ArchivoConsulta::buscarPorRangoFechaMedicamento(){
-    Consulta reg;
-    int anioMin,anioMax;
-    cout << "Ingrese el anio minimo del inicio del medicamento: ";
-    cin >> anioMin;
-    cout << endl;
-    cout << "Ingrese el anio maximo del inicio del medicamento: ";
-    cin >> anioMax;
-    cout << endl;
-
-    int cant = cantidadRegistro();
-    if(cant <= 0){
-        cout << "No hay registros para mostrar" << endl;
-        return false;
-    }
-
-    int cont = 1;
-    int alMenosUno = 0;
-
-    for(int i = 0; i < cant; i++){
-        reg = leerRegistro(i);
-        if(reg.getFechaInicioMedicamento().getAnio() >= anioMin && reg.getFechaInicioMedicamento().getAnio() <= anioMax && reg.getIDMedicamento() > 0){
-            cout << "REGISTRO N" << cont << ":" << endl;
-            reg.mostrar();
-            cont++;
-            alMenosUno = 1;
-            cout << endl;
-        }
-    }
-
-    if(alMenosUno == 0){
-        cout << "No hay registros con el inicio de medicacion entre esos anios" << endl;
-        return false;
-    }
-
-    return true;
-
-}
-
-bool ArchivoConsulta::buscarPorRangoFechaConsulta(){
-    Consulta reg;
-    int anioMin,anioMax;
-    cout << "Ingrese el anio minimo de la consulta: ";
-    cin >> anioMin;
-    cout << endl;
-    cout << "Ingrese el anio maximo de la consulta: ";
-    cin >> anioMax;
-    cout << endl;
-
-    int cant = cantidadRegistro();
-    if(cant <= 0){
-        cout << "No hay registros para mostrar" << endl;
-        return false;
-    }
-
-    int cont = 1;
-    int alMenosUno = 0;
-
-    for(int i = 0; i < cant; i++){
-        reg = leerRegistro(i);
-        if(reg.getFechaConsulta().getAnio() >= anioMin && reg.getFechaConsulta().getAnio() <= anioMax){
-            cout << "REGISTRO N" << cont << ":" << endl;
-            reg.mostrar();
-            cont++;
-            alMenosUno = 1;
-            cout << endl;
-        }
-    }
-
-    if(alMenosUno == 0){
-        cout << "No hay registros con el inicio de medicacion entre esos anios" << endl;
-        return false;
-    }
-
-    return true;
-}
-
-bool ArchivoConsulta::buscarPorRangoPrecioConsulta(){
-    Consulta reg;
-    float precioMin,precioMax;
-    cout << "Ingrese el precio minimo de la consulta: $";
-    cin >> precioMin;
-    cout << endl;
-    cout << "Ingrese el precio maximo de la consulta: $";
-    cin >> precioMax;
-    cout << endl;
-
-    int cant = cantidadRegistro();
-    if(cant <= 0){
-        cout << "No hay registros para mostrar" << endl;
-        return false;
-    }
-
-    int cont = 1;
-    int alMenosUno = 0;
-
-    for(int i = 0; i < cant; i++){
-        reg = leerRegistro(i);
-        if(reg.getPrecioConsulta() >= precioMin && reg.getPrecioConsulta() <= precioMax){
-            cout << "REGISTRO N" << cont << ":" << endl;
-            reg.mostrar();
-            cont++;
-            alMenosUno = 1;
-            cout << endl;
-        }
-    }
-
-    if(alMenosUno == 0){
-        cout << "No hay registros con el inicio de medicacion entre esos anios" << endl;
-        return false;
-    }
-
-    return true;
-}
-
-bool ArchivoConsulta::buscarPorIDMedicamento(){
-    Consulta reg;
-    int id;
-    cout << "Ingrese el ID del medicamento: ";
-    cin >> id;
-    cout << endl;
-
-    int cant = cantidadRegistro();
-    if(cant <= 0){
-        cout << "No hay registros para mostrar" << endl;
-        return false;
-    }
-
-    int cont = 1;
-    int alMenosUno = 0;
-
-    for(int i = 0; i < cant; i++){
-        reg = leerRegistro(i);
-        if(reg.getIDMedicamento() == id){
-            cout << "REGISTRO N" << cont << ":" << endl;
-            reg.mostrar();
-            cont++;
-            alMenosUno = 1;
-            cout << endl;
-        }
-    }
-
-    if(alMenosUno == 0){
-        cout << "No hay registros con el inicio de medicacion entre esos anios" << endl;
-        return false;
-    }
-
-    return true;
-}
-
-bool ArchivoConsulta::copiaSeguridad(){
-    Consulta reg;
-
-     FILE *pCo;
-     FILE *pConsulta;
-
-     pCo = fopen("copiaConsulta.dat","wb");
-     pConsulta = fopen(nombre,"rb");
-
-     if(pCo == nullptr || pConsulta == nullptr){
-        fclose(pConsulta);
-        fclose(pCo);
+     if(pMo == nullptr || pMon == nullptr){
+        fclose(pMon);
+        fclose(pMo);
         cout << "Error en memoria" << endl;
         return false;
      }
 
-     while(fread(&reg,tamanioRegistro,1,pConsulta)==1){
-        fwrite(&reg,tamanioRegistro,1,pCo);
+     while(fread(&reg,tamanioRegistro,1,pMon)==1){
+        fwrite(&reg,tamanioRegistro,1,pMo);
      }
 
-     fclose(pConsulta);
-     fclose(pCo);
+     fclose(pMon);
+     fclose(pMo);
 
      return true;
 }
 
-bool ArchivoConsulta::reestablecer(){
-    Consulta reg;
+bool ArchivoMonstruo::reestablecer(){
+    Monstruo reg;
 
-     FILE *pCo;
-     FILE *pConsulta;
+     FILE *pMo;
+     FILE *pMon;
 
-     pCo = fopen("copiaConsulta.dat","rb");
-     pConsulta = fopen(nombre,"wb");
+     pMo = fopen("copiaMonstruos.dat","rb");
+     pMon = fopen(nombre,"wb");
 
-     if(pCo == nullptr || pConsulta == nullptr){
-        fclose(pConsulta);
-        fclose(pCo);
+     if(pMo == nullptr || pMon == nullptr){
+        fclose(pMon);
+        fclose(pMo);
         cout << "Error en memoria" << endl;
         return false;
      }
 
-    while(fread(&reg,tamanioRegistro,1,pCo)==1){
-        fwrite(&reg,tamanioRegistro,1,pConsulta);
+    while(fread(&reg,tamanioRegistro,1,pMo)==1){
+        fwrite(&reg,tamanioRegistro,1,pMon);
      }
 
-     fclose(pConsulta);
-     fclose(pCo);
+     fclose(pMon);
+     fclose(pMo);
 
      return true;
 }
